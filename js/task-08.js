7de83dcb2e9b0690a208dc89a837b93bc6df6ea2
ganna1008 <ganna1008@gmail.com>
2023-01-16 21:29:48 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+
+    if (!email || !password) {
+        alert('All inputs must not be empty');
+    }
+    else {
+        const formOutput = { email, password };
+        console.log(formOutput);
+        formEl.reset();
+    }
+
+}
