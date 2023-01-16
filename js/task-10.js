19d88c7d4098f87c9104534afd17c827a12c426b
ganna1008 <ganna1008@gmail.com>
2023-01-16 23:51:03 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,9 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-console.log('hello')
-
 const boxEl = document.getElementById('boxes');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
